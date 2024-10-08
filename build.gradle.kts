578c880b890011b0ef449f817ca4085cd78ca583
Lyneon <69835744+Lyneon@users.noreply.github.com>
2024-10-08 14:46:35 +0800

    Create Project

---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    id("java")
+}
+
+group = "team.jvav"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+}
---
+++ src/main/java/team/jvav/stuhub/Main.java
@@ -0,0 +1,7 @@
+package team.jvav.stuhub;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("StuHub");
+    }
+}
