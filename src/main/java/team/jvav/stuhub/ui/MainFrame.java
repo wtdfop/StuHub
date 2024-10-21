6e0c4c97b97cc147f6ded78c0d96c7ab5e31731f
tw ffrwg <150423613+wtdfop@users.noreply.github.com>
2024-10-21 20:59:31 +0800

    完成UI

--- src/main/java/team/jvav/stuhub/ui/MainFrame.java
+++ src/main/java/team/jvav/stuhub/ui/MainFrame.java
@@ -1,83 +1,65 @@
 package team.jvav.stuhub.ui;
 
-import team.jvav.stuhub.Main;
-import team.jvav.stuhub.data.model.Group;
-import team.jvav.stuhub.data.model.Student;
-
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import team.jvav.stuhub.data.constants.*;
 
+/**
+ * MainFrame类为主窗口，包含主要功能和组件
+ */
 public class MainFrame extends JFrame {
-    public JButton drawStudentFromClassButton, drawGroupFromClassButton, drawStudentFromGroupButton;
-    Container contentPane;
-    JLabel classStudentLabel, groupStudentLabel, groupLabel;
+    /**
+     * 主窗口欢迎语
+     */
+    JLabel nameLabel = new JLabel();
 
+    /**
+     * 窗口跳转按钮
+     */
+    JButton manageButton = new JButton("Manage students");
+    JButton ShowButton = new JButton("Show students and group");
+
+    /**
+     * 构造方法
+     */
     public MainFrame() {
         super("StuHub");
 
-        contentPane = getContentPane(); // 使用 'getContentPane()' 获取内容面板
-        contentPane.setLayout(null);
-
-        drawStudentFromClassButton = new JButton("Draw Student From Class");
-        drawStudentFromClassButton.setBounds(400, 50, 200, 50);
-        contentPane.add(drawStudentFromClassButton);
-
-        drawGroupFromClassButton = new JButton("Draw Group From Class");
-        drawGroupFromClassButton.setBounds(400, 150, 200, 50);
-        contentPane.add(drawGroupFromClassButton);
-
-        drawStudentFromGroupButton = new JButton("Draw Student From Group");
-        drawStudentFromGroupButton.setBounds(400, 250, 200, 50);
-        contentPane.add(drawStudentFromGroupButton);
-
-        Dimension size = Toolkit.getDefaultToolkit().getScreenSize(); // 使用 Toolkit 获取屏幕尺寸
-        setSize(size.width / 2, size.height / 2);
+        // 设置主窗口布局
+        setLayout(new BorderLayout());
+        setBounds(FrameConstants.screenSize.height / 2, FrameConstants.screenSize.height / 2, FrameConstants.screenSize.width / 2, FrameConstants.screenSize.height / 2);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
+
+        // 创建按钮容器
+        JPanel buttonPanel = new JPanel(new FlowLayout());
+        buttonPanel.add(manageButton);
+        buttonPanel.add(ShowButton);
+
+        // 设置按钮的监听事件
+        manageButton.addActionListener(e -> {
+            new ManageInformationFrame(this);
+        });
+        ShowButton.addActionListener(e -> {
+
+        });
+//        manageButton.addActionListener(new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                 new ShowFunctionFrame(this);
+//            }
+//        });
+//        ShowButton.addActionListener(new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//
+//            }
+//        });
+
+        // 设置按钮和欢迎词的位置
+        add(buttonPanel, BorderLayout.SOUTH);
+        add(nameLabel, BorderLayout.CENTER);
+
+        // 其他设置
         setVisible(true);
-
-        drawStudentFromClassButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                Student t = Main.c.getRandomStudent();
-                if (classStudentLabel != null) {
-                    contentPane.remove(classStudentLabel);
-                }
-                classStudentLabel = new JLabel(t.getName());
-                classStudentLabel.setBounds(300, 50, 100, 50);
-                contentPane.add(classStudentLabel);
-                contentPane.revalidate(); // 更新布局
-                contentPane.repaint(); // 重绘内容面板
-            }
-        });
-
-        drawGroupFromClassButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                Group t = Main.c.getRandomGroup();
-                if (groupLabel != null) {
-                    contentPane.remove(groupLabel);
-                }
-                groupLabel = new JLabel(t.getName());
-                groupLabel.setBounds(300, 150, 100, 50);
-                contentPane.add(groupLabel);
-                contentPane.revalidate();
-                contentPane.repaint();
-            }
-        });
-
-        drawStudentFromGroupButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                Student t = Main.c.getGroup(2).getRandomStudent();
-                if (groupStudentLabel != null) {
-                    contentPane.remove(groupStudentLabel);
-                }
-                groupStudentLabel = new JLabel(t.getName());
-                groupStudentLabel.setBounds(300, 250, 100, 50);
-                contentPane.add(groupStudentLabel);
-                contentPane.revalidate();
-                contentPane.repaint();
-            }
-        });
     }
 }
